c755d3dfcbd3bf144d989bb3c3a3b2d73cd7d61b
Gabriel Antunes <antunes.t.gabriel@gmail.com>
2024-10-18 19:40:37 +0000

    Charts e Index

--- pages/charts/index.tsx
+++ pages/charts/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <div>Oi</div>
+        <div>Charts</div>
       </main>
     </div>
   );
