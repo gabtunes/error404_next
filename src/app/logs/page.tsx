b143fcbfd80430f99ba806a887337f179960c18b
gabtunes <antunes.t.gabriel@gmail.com>
2024-11-18 03:00:58 -0300

    Errinhos Vercel 7

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -1,25 +1,36 @@
-export default async function Page() {
-    const fetchLogs = async () => {
-        const res = await fetch(process.env.VERCEL_URL + "/api/logs");
-        const logs = await res.json();
-        return logs;
+"use client"
+
+import useSWR from "swr";
+
+const fetcher = (url: string) =>
+    fetch(url)
+        .then((res) => res.json());
+
+export default function Page() {
+    const {
+        data: logs,
+        error,
+        isLoading,
+    } = useSWR('/api/logs/', fetcher);
+
+    if (error) return <p>Falhou</p>;
+    if (isLoading) return <p>Carregando...</p>;
+    if (!logs) return null;
+    if (logs) {
+        return (
+            <div>
+                <h1>Logs</h1>
+                {
+                    logs.map((log: any) => (
+                        <div key={log._id}>
+                            <h2>{log.membro}</h2>
+                            <p>{log.tmdb}</p>
+                            <h2>{log.nota}</h2>
+                            <h2>{log.data}</h2>
+                        </div>
+                    ))
+                }
+            </div>
+        )
     }
-
-    const logs = await fetchLogs();
-
-    return (
-        <div>
-            <h1>Logs</h1>
-            {
-                logs.map((log: any) => (
-                    <div key={log._id}>
-                        <h2>{log.membro}</h2>
-                        <p>{log.tmdb}</p>
-                        <h2>{log.nota}</h2>
-                        <h2>{log.data}</h2>
-                    </div>
-                ))
-            }
-        </div>
-    )
 }
