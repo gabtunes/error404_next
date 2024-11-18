03ee3585ee9958ad058b14c0913b252643a4c8bc
gabtunes <antunes.t.gabriel@gmail.com>
2024-11-18 02:05:56 -0300

    Errinhos Vercel 5

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -1,6 +1,6 @@
 export default async function Page() {
     const fetchLogs = async () => {
-        const res = await fetch("/api/logs");
+        const res = await fetch(process.env.VERCEL_URL + "api/logs");
         const logs = await res.json();
         return logs;
     }
