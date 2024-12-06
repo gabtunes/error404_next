b9a92c09afe91d86efb96a07bf98e691930d84f1
Gabriel Antunes <antunes.t.gabriel@gmail.com>
2024-12-06 14:51:25 +0000

    Teste 15 com fetch

--- src/app/charts/[ano]/page.tsx
+++ src/app/charts/[ano]/page.tsx
@@ -1,25 +1,25 @@
-//import { getChartComparison } from "@/infra/charts";
-//import { getCharts } from "@/infra/log";
+import { getChartComparison } from "@/infra/charts";
+import { getCharts } from "@/infra/log";
 import { getBubbling } from "@/infra/bubbling";
 import Link from "next/link";
-//import Charts from "@/components/charts";
+import Charts from "@/components/charts";
 import Filme from "@/components/filme";
 
 export default async function Page({ params, }: { params: Promise<{ ano: string }> }) {
     const anos = [2024, 2023, 2022, 2021];
     const ano_uri = (await params).ano;
-    //const res = await getChartComparison(parseInt(ano_uri));
-    //const charts = await res.json();
+    const res = await getChartComparison(parseInt(ano_uri));
+    const charts = await res.json();
 
-    //const ultimo = charts[0].limite;
-    //let penultimo: any = [];
+    const ultimo = charts[0].limite;
+    let penultimo: any = [];
 
-    //if (charts.length > 1) {
-        //penultimo = charts[1].chart;
-    //}
+    if (charts.length > 1) {
+        penultimo = charts[1].chart;
+    }
 
-    //const res2 = await getCharts(parseInt(ano_uri));
-    //const last_chart = await res2.json();
+    const res2 = await getCharts(parseInt(ano_uri));
+    const last_chart = await res2.json();
 
     const res3 = await getBubbling();
     const bubbling = await res3.json();
@@ -40,9 +40,9 @@ export default async function Page({ params, }: { params: Promise<{ ano: string 
             {(bubbling) &&
                 <Filme filme={bubbling[0].titulo} />
             }
-            {/*(last_chart && charts && bubbling) &&
+            {last_chart &&
                 <Charts atual={false} ultimo={ultimo} penultimo={penultimo} last_chart={last_chart} />
-            */}
+            }
         
         </div>
     )
