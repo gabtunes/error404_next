058f79db4475af64585d1f718a66906476313d41
gabtunes <antunes.t.gabriel@gmail.com>
2024-12-16 00:04:06 -0300

    Top albuns implementado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/charts"><Banner img="CHARTS 2024" /></Link>
-      <Link href=""><Banner img="TOP ALBUNS 2024 (EM BREVE)" /></Link>
+    <div className="flex flex-col items-center">
+      <div className="w-full md:w-[600px] grid grid-cols-2 gap-[5px] p-[5px]">
+        <Link href="/charts"><Banner img="CHARTS 2024" /></Link>
+        <Link href="/bolao"><Banner img="BOLÃO 2025" /></Link>
+        <Link href="/"><Banner img="TOP ALBUNS 2024" /></Link>
+      </div>
     </div>
   );
 }
