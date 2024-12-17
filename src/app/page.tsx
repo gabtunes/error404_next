a8f46e9b84fbbb6706ca19c1c6557f592b6915b9
Gabriel Antunes <antunes.t.gabriel@gmail.com>
2024-12-17 16:42:19 +0000

    Identificar pelo nome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,12 +35,14 @@ export default function Home() {
     return (
       <div className="flex flex-col items-center">
         {data &&
-          <div>{data.id}</div>
+          <div>Olá, {data.first_name}</div>
         }
         <div className="w-full md:w-[600px] grid grid-cols-2 gap-[5px] p-[5px]">
           <Link href="/charts"><Banner img="CHARTS 2024" /></Link>
           <Link href="/bolao"><Banner img="BOLÃO 2025" /></Link>
-          <Link href="/"><Banner img="TOP ALBUNS 2024" /></Link>
+          {data &&
+            <Link href="/musica"><Banner img="MÚSICA 2024" /></Link>
+          }
         </div>
       </div>
     );
