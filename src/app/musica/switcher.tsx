e30b90eb080c5b7bcf46902df3cb100ba729a342
Gabriel Antunes <antunes.t.gabriel@gmail.com>
2024-12-19 02:14:49 +0000

    Return no lugar certo

--- src/app/musica/switcher.tsx
+++ src/app/musica/switcher.tsx
@@ -38,28 +38,29 @@ export default function Switcher(props: {data: Array<IMusica>}) {
 
     if (!isLoaded)
         return <div>Loading...</div>
-    else {
-        let data_membro: any[] = []
+    else {        
         if(user){
-            data_membro = data.filter((registro: any) => registro["membro"] == user.id)
+            const data_membro = data.filter((registro: any) => registro["membro"] == user.id)
+
+            return (
+                <div className="flex flex-col items-center">
+                    <div>{user?.id}</div>
+                    <div className="flex flex-row gap-5">
+                        <div className={`p-3 ${page == "albums" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("albums") }}>Top Álbuns</div>
+                        <div className={`p-3 ${page == "musicas" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("musicas") }}>Top Músicas</div>
+                    </div>
+                    <div className={`${page == "albums" ? "" : "hidden"}`}>
+                        <TopAlbums albums_db={data_membro} />
+                    </div>
+                    <div className={`${page == "musicas" ? "" : "hidden"}`}>
+                        <TopTracks />
+                    </div>
+                </div>
+            )
         } else {
             redirect("/")
         }
 
-        return (
-            <div className="flex flex-col items-center">
-                <div>{user?.id}</div>
-                <div className="flex flex-row gap-5">
-                    <div className={`p-3 ${page == "albums" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("albums") }}>Top Álbuns</div>
-                    <div className={`p-3 ${page == "musicas" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("musicas") }}>Top Músicas</div>
-                </div>
-                <div className={`${page == "albums" ? "" : "hidden"}`}>
-                    <TopAlbums albums_db={data_membro} />
-                </div>
-                <div className={`${page == "musicas" ? "" : "hidden"}`}>
-                    <TopTracks />
-                </div>
-            </div>
-        )
+        
     }
 }
