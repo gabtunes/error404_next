7834eca0daa80ba2a3e6ce40f5997f8d6d6a1139
Gabriel Antunes <antunes.t.gabriel@gmail.com>
2024-12-20 18:57:52 +0000

    TelegramApp via context

--- src/app/musica/switcher.tsx
+++ src/app/musica/switcher.tsx
@@ -20,7 +20,7 @@ interface UserData {
     is_premium?: boolean
 }
 
-export default function Switcher(props: {data: Array<IMusica>}) {
+export default function Switcher(props: { data: Array<IMusica> }) {
     const [page, setPage] = useState("albums")
     const [user, setUser] = useState<UserData | null>(null)
     const [isLoaded, setIsLoaded] = useState(false)
@@ -33,34 +33,26 @@ export default function Switcher(props: {data: Array<IMusica>}) {
         setIsLoaded(true)
     }, [])
 
-    
-
 
     if (!isLoaded)
         return <div>Loading...</div>
-    else {        
-        if(user){
-            const data_membro = data.filter((registro: any) => registro["membro"] == user.id)
-
-            return (
-                <div className="flex flex-col items-center">
-                    <div>{user?.id}</div>
-                    <div className="flex flex-row gap-5">
-                        <div className={`p-3 ${page == "albums" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("albums") }}>Top Álbuns</div>
-                        <div className={`p-3 ${page == "musicas" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("musicas") }}>Top Músicas</div>
-                    </div>
-                    <div className={`${page == "albums" ? "" : "hidden"}`}>
-                        <TopAlbums albums_db={data_membro} />
-                    </div>
-                    <div className={`${page == "musicas" ? "" : "hidden"}`}>
-                        <TopTracks />
-                    </div>
+    else if(user) {
+        console.log(user)
+        return (
+            <div className="flex flex-col items-center">
+                <div>{user?.id}</div>
+                <div className="flex flex-row gap-5">
+                    <div className={`p-3 ${page == "albums" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("albums") }}>Top Álbuns</div>
+                    <div className={`p-3 ${page == "musicas" ? "bg-red-500" : "bg-white"}`} onClick={() => { setPage("musicas") }}>Top Músicas</div>
                 </div>
-            )
-        } else {
-            redirect("/")
-        }
-
-        
+                <div className={`${page == "albums" ? "" : "hidden"}`}>
+                    <TopAlbums albums_db={data.filter((registro: any) => registro["membro"] == user?.id)} />
+                    
+                </div>
+                <div className={`${page == "musicas" ? "" : "hidden"}`}>
+                    <TopTracks />
+                </div>
+            </div>
+        )
     }
 }
