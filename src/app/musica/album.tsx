ea54702349ff67df8eb24617c3dcd04dc6f0a03c
gabtunes <antunes.t.gabriel@gmail.com>
2024-12-21 00:39:10 -0300

    Teste #32 - Dois useEffect com parâmetros diferentes

--- src/app/musica/album.tsx
+++ src/app/musica/album.tsx
@@ -26,18 +26,22 @@ export default function TopAlbums(props: { albums_db: Array<IMusica> }) {
     const membro: number | undefined = user?.id
 
     const [top_db, setTopDB] = useState<object[]>([])
-    const [top, setTop] = useState<object[]>([])
-
+    
     useEffect(() => {
         if (membro) {
             const filtro = props["albums_db"].filter((registro: any) => registro["membro"] == membro)
             console.log(filtro)
             if (filtro.length > 0) {
                 setTopDB(filtro[0].albums)
-                setTop(filtro[0].albums)
             }
         }
-    }, [top_db, top])
+    })
+
+    const [top, setTop] = useState<object[]>([])
+
+    useEffect(() => {
+        setTop(top_db)
+    }, [])
 
     const [albums, setAlbums] = useState([])
     const [showList, setShowList] = useState(false)
