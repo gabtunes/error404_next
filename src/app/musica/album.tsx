b7108b027d86759b2c1fca56b204a2ec8f17577e
gabtunes <antunes.t.gabriel@gmail.com>
2024-12-20 22:11:09 -0300

    Teste #8 - Top_db salva 2?

--- src/app/musica/album.tsx
+++ src/app/musica/album.tsx
@@ -25,8 +25,7 @@ export default function TopAlbums(props: {albums_db: Array<IMusica>}) {
 
     const membro: number | undefined = user?.id
     const filtro = props["albums_db"].filter((registro: any) => registro["membro"] == membro)
-    console.log(filtro)
-    
+
     let top_db: Array<object>
 
     if(filtro.length > 0){
@@ -39,7 +38,7 @@ export default function TopAlbums(props: {albums_db: Array<IMusica>}) {
     const [albums, setAlbums] = useState([])
     const [top, setTop] = useState<any[]>(top_db)
     const [showList, setShowList] = useState(false)
-
+    console.log(top)
     /* eslint-disable */
     const LastFM = require('last-fm')
     /* eslint-disable */
@@ -70,86 +69,92 @@ export default function TopAlbums(props: {albums_db: Array<IMusica>}) {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center gap-5 mt-5" >
-            <div>{membro}</div>
-            <input placeholder="Álbum, artista..." className="border-black border-2 p-2 w-4/5 md:w-[250px] lg:w=[300px] h-[40px]" onChange={handleChange}></input>
-            <div className="justify-center gap-2 grid grid-cols-2 md:grid-cols-3 place-content-center">
-                {albums &&
-                    albums.map((album: any) => (
-                        (album.images.length != 0) &&
-                        <SearchContext.Provider value={{top, setTop}}  key={album.artistName + "_" + album.name} >
-                            <ResultAlbum album={album}/>
-                        </SearchContext.Provider>
-                    ))
-                }
-            </div>
-
-
-            <div className={`p-5 h-full w-[150px] bg-white drop-shadow-xl ${showList ? "right-0" : "-right-[150px]"} duration-[800ms] top-0 fixed flex flex-col gap-5 items-center`}>
-                <button onClick={handleSave} className={`z-0 absolute -left-18 bg-white text-center ${showList ? "bottom-20" : "bottom-5 "} duration-[400ms] flex items-center justify-center rounded-full drop-shadow-sm size-[50px]`}>
-                    <span className="material-icons">
-                    {
-                        JSON.stringify(top_db) == JSON.stringify(top) ?
-                        "check" :
-                        "save"
+        <>
+        {
+            user ?
+            (<div className="flex flex-col items-center justify-center gap-5 mt-5" >
+                <div>{membro}</div>
+                <input placeholder="Álbum, artista..." className="border-black border-2 p-2 w-4/5 md:w-[250px] lg:w=[300px] h-[40px]" onChange={handleChange}></input>
+                <div className="justify-center gap-2 grid grid-cols-2 md:grid-cols-3 place-content-center">
+                    {albums &&
+                        albums.map((album: any) => (
+                            (album.images.length != 0) &&
+                            <SearchContext.Provider value={{top, setTop}}  key={album.artistName + "_" + album.name} >
+                                <ResultAlbum album={album}/>
+                            </SearchContext.Provider>
+                        ))
                     }
-                    </span>
-                </button>
-                <button className="z-10 absolute bottom-5 -left-18 bg-white text-center flex items-center justify-center rounded-full drop-shadow-sm size-[50px]" onClick={() => {
-                    setShowList(!showList)
-                }}>
-                    <span className="material-icons">
+                </div>
+    
+    
+                <div className={`p-5 h-full w-[150px] bg-white drop-shadow-xl ${showList ? "right-0" : "-right-[150px]"} duration-[800ms] top-0 fixed flex flex-col gap-5 items-center`}>
+                    <button onClick={handleSave} className={`z-0 absolute -left-18 bg-white text-center ${showList ? "bottom-20" : "bottom-5 "} duration-[400ms] flex items-center justify-center rounded-full drop-shadow-sm size-[50px]`}>
+                        <span className="material-icons">
                         {
-                            showList
-                            ?
-                            "close"
-                            :
-                            "leaderboard"
-                        }                        
-                    </span>
-                </button>
-                {
-                    top.map((album: any, index: any) => (
-                        <div className="flex flex-col gap-2" key={index}>
-                            <div className="relative size-[100px] flex items-end">
-                                <span style={{ textShadow: '#000 1px 0 10px' }} className="leading-none z-10 absolute funnel-sans text-[65px] text-white">{index + 1}</span>
-                                <img className="z-0 absolute size-[100px]" src={album.images[2]}></img>
-                            </div>
-                            <div className="grid grid-cols-3">
-                                {(index != 0) &&
-                                    <button className="col-start-1 material-icons" onClick={() => {
+                            JSON.stringify(top_db) == JSON.stringify(top) ?
+                            "check" :
+                            "save"
+                        }
+                        </span>
+                    </button>
+                    <button className="z-10 absolute bottom-5 -left-18 bg-white text-center flex items-center justify-center rounded-full drop-shadow-sm size-[50px]" onClick={() => {
+                        setShowList(!showList)
+                    }}>
+                        <span className="material-icons">
+                            {
+                                showList
+                                ?
+                                "close"
+                                :
+                                "leaderboard"
+                            }                        
+                        </span>
+                    </button>
+                    {
+                        top.map((album: any, index: any) => (
+                            <div className="flex flex-col gap-2" key={index}>
+                                <div className="relative size-[100px] flex items-end">
+                                    <span style={{ textShadow: '#000 1px 0 10px' }} className="leading-none z-10 absolute funnel-sans text-[65px] text-white">{index + 1}</span>
+                                    <img className="z-0 absolute size-[100px]" src={album.images[2]}></img>
+                                </div>
+                                <div className="grid grid-cols-3">
+                                    {(index != 0) &&
+                                        <button className="col-start-1 material-icons" onClick={() => {
+                                            const currentTop = [...top]
+                                            const anterior = currentTop[index - 1]
+                                            currentTop[index - 1] = currentTop[index]
+                                            currentTop[index] = anterior
+                                            setTop(currentTop)
+                                            //checkSave()
+                                        }}>keyboard_arrow_up</button>
+                                    }
+                                    <button className="col-start-2 material-icons" onClick={() => {
                                         const currentTop = [...top]
-                                        const anterior = currentTop[index - 1]
-                                        currentTop[index - 1] = currentTop[index]
-                                        currentTop[index] = anterior
+                                        currentTop.splice(index, 1)
                                         setTop(currentTop)
                                         //checkSave()
-                                    }}>keyboard_arrow_up</button>
-                                }
-                                <button className="col-start-2 material-icons" onClick={() => {
-                                    const currentTop = [...top]
-                                    currentTop.splice(index, 1)
-                                    setTop(currentTop)
-                                    //checkSave()
-                                }}>delete</button>
-
-                                {(index != top.length - 1) &&
-                                    <button className="col-start-3 material-icons" onClick={() => {
-                                        const currentTop = [...top]
-                                        const posterior = currentTop[index + 1]
-                                        currentTop[index + 1] = currentTop[index]
-                                        currentTop[index] = posterior
-                                        setTop(currentTop)
-                                        //checkSave()
-                                    }}>keyboard_arrow_down</button>
-                                }
+                                    }}>delete</button>
+    
+                                    {(index != top.length - 1) &&
+                                        <button className="col-start-3 material-icons" onClick={() => {
+                                            const currentTop = [...top]
+                                            const posterior = currentTop[index + 1]
+                                            currentTop[index + 1] = currentTop[index]
+                                            currentTop[index] = posterior
+                                            setTop(currentTop)
+                                            //checkSave()
+                                        }}>keyboard_arrow_down</button>
+                                    }
+                                </div>
+    
                             </div>
-
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
+                        ))
+                    }
+                </div>
+            </div>) :
+            (<div></div>)
+        }
+        </>
     )
 }
 
