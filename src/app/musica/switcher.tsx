3a1a0b39bc73e2df098da620ff08381294dc028d
gabtunes <antunes.t.gabriel@gmail.com>
2024-12-21 21:36:40 -0300

    Gap redesigned

--- src/app/musica/switcher.tsx
+++ src/app/musica/switcher.tsx
@@ -12,7 +12,7 @@ export default function Switcher(props: { data: Array<IMusica> }) {
 
     return (
         <div className="flex flex-col items-center" >
-            <div className="flex flex-row gap-5">
+            <div className="flex flex-row">
                 <div className={`p-3 ${page == "albums" ? "text-[var(--tg-theme-button-text-color)] bg-[var(--tg-theme-button-color)]" : "text-[var(--tg-theme-text-color)] bg-[var(--tg-theme-bg-color)]"}`} onClick={() => { setPage("albums") }}>Top Álbuns</div>
                 <div className={`p-3 ${page == "musicas" ? "text-[var(--tg-theme-button-text-color)] bg-[var(--tg-theme-button-color)]" : "text-[var(--tg-theme-text-color)] bg-[var(--tg-theme-bg-color)]"}`} onClick={() => { setPage("musicas") }}>Top Músicas</div>
             </div>
