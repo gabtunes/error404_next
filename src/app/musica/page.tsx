8cfe5dc2418c18aed292e82a7d3c76aae3071e47
gabtunes <antunes.t.gabriel@gmail.com>
2025-04-12 00:34:40 -0300

    Sem botao

--- src/app/musica/page.tsx
+++ src/app/musica/page.tsx
@@ -1,14 +1,13 @@
-import Botao from "@/components/botao";
 import Link from "next/link";
 
 export default async function Page() {
     return (
      <div className="flex flex-row flex-wrap justify-between gap-3 p-3">
         <Link href="/musica/lists">
-            <Botao titulo="Listas" />
+            <div>Listas</div>
         </Link>
         <Link href="/musica/createList">
-            <Botao titulo={"Meu " + new Date().getFullYear()} />
+            <div>{"Meu " + new Date().getFullYear()}</div>
         </Link>
      </div>   
     )
